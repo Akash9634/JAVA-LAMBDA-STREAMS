f7fcbf2593666c03176b4b377f793404dc3fcb3e
Akash9634 <akashkush2020@gmail.com>
2026-01-14 11:44:04 +0530

    new commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,13 @@ import org.example.LambdaExpression.SoftwareEngineer;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Employee employee = new SoftwareEngineer();
-        System.out.println(employee.getName());
+        Employee sde = () -> "Software engineer";
+        System.out.println(sde.getName());
+        Employee editor = () -> "Editor";
+        System.out.println(editor.getName());
+
+        //we don't need separate implementation class for editor or sde if interface has only one abstract method
+        //but if class having multiple methods default methods , and we want to override them then we would need separate implementation class
+
     }
 }
